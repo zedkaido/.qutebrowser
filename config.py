ad11ecbfae51fbbfa13eb262889f99180122db60
Zed Kaido <ekai@tuta.io>
2023-08-20 16:52:42 +0100

    tidy up `config.py`

--- config.py
+++ config.py
@@ -1,100 +1,24 @@
 #!/usr/bin/env python3
 
+# https://qutebrowser.org/doc/help/settings.html
 config.load_autoconfig()
 
 c.url.default_page = 'about:blank'
 c.url.start_pages = 'about:blank'
 
-c.aliases = {'q': 'quit', 'w': 'session-save', 'wq': 'quit --save'}
+c.auto_save.session = True
+c.session.lazy_restore = True
 
-#config.set("colors.webpage.darkmode.enabled", True)
+c.editor.command = [
+    "alacritty",
+    "-e",
+    "nvim",
+    "-f",
+    "{file}",
+    "-c",
+    "normal {line}G{column0}1",
+]
 
-config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
-config.set('content.cookies.accept', 'all', 'devtools://*')
-config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
-
-c.downloads.location.directory = '~/Downloads'
-c.content.fullscreen.window = True
-c.window.title_format = '{perc}{current_title}' 
-
-# Which cookies to accept. With QtWebEngine, this setting also controls
-# other features with tracking capabilities similar to those of cookies;
-# including IndexedDB, DOM storage, filesystem API, service workers, and
-# AppCache. Note that with QtWebKit, only `all` and `never` are
-# supported as per-domain values. Setting `no-3rdparty` or `no-
-# unknown-3rdparty` per-domain on QtWebKit will have the same effect as
-# `all`.
-# Type: String
-# Valid values:
-#   - all: Accept all cookies.
-#   - no-3rdparty: Accept cookies from the same origin only. This is known to break some sites, such as GMail.
-#   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
-#   - never: Don't accept cookies at all.
-config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
-config.set('content.cookies.accept', 'all', 'devtools://*')
-config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
-config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://accounts.google.com/*')
-config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36', 'https://*.slack.com/*')
-config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://docs.google.com/*')
-config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://drive.google.com/*')
-
-# Load images automatically in web pages.
-# Type: Bool
-config.set('content.images', True, 'chrome-devtools://*')
-
-# Load images automatically in web pages.
-# Type: Bool
-config.set('content.images', True, 'devtools://*')
-
-# Enable JavaScript.
-# Type: Bool
-config.set('content.javascript.enabled', True, 'chrome-devtools://*')
-
-# Enable JavaScript.
-# Type: Bool
-config.set('content.javascript.enabled', True, 'devtools://*')
-
-# Enable JavaScript.
-# Type: Bool
-config.set('content.javascript.enabled', True, 'chrome://*/*')
-
-# Enable JavaScript.
-# Type: Bool
-config.set('content.javascript.enabled', True, 'qute://*/*')
-
-
-# Directory to save downloads to. If unset, a sensible OS-specific
-# default is used.
-# Type: Directory
-c.downloads.location.directory = '~/Downloads'
-
-# When to show the tab bar.
-# Type: String
-# Valid values:
-#   - always: Always show the tab bar.
-#   - never: Always hide the tab bar.
-#   - multiple: Hide the tab bar if only one tab is open.
-#   - switching: Show the tab bar when switching tabs.
-c.tabs.show = 'multiple'
-
-# Search engines which can be used via the address bar.  Maps a search
-# engine name (such as `DEFAULT`, or `ddg`) to a URL with a `{}`
-# placeholder. The placeholder will be replaced by the search term, use
-# `{{` and `}}` for literal `{`/`}` braces.  The following further
-# placeholds are defined to configure how special characters in the
-# search terms are replaced by safe characters (called 'quoting'):  *
-# `{}` and `{semiquoted}` quote everything except slashes; this is the
-# most   sensible choice for almost all search engines (for the search
-# term   `slash/and&amp` this placeholder expands to `slash/and%26amp`).
-# * `{quoted}` quotes all characters (for `slash/and&amp` this
-# placeholder   expands to `slash%2Fand%26amp`). * `{unquoted}` quotes
-# nothing (for `slash/and&amp` this placeholder   expands to
-# `slash/and&amp`).  The search engine named `DEFAULT` is used when
-# `url.auto_search` is turned on and something else than a URL was
-# entered to be opened. Other search engines can be used by prepending
-# the search engine name to the search term, e.g. `:open google
-# qutebrowser`.
-# Type: Dict
 c.url.searchengines = {
     'DEFAULT': 'https://google.com/search?q={}',
     'cows': 'https://swisscows.com/web/query?={}',
@@ -102,12 +26,29 @@ c.url.searchengines = {
     's': 'https://startpage.com/sp/search?q={}',
     'g': 'https://google.com/search?q={}',
     'd': 'https://duckduckgo.com/?q={}',
-    'git': 'https://gitlab.com/search?search={}',
+    'gl': 'https://gitlab.com/search?search={}',
+    'gh': 'https://github.com/search?q={}',
     'h': 'https://hoogle.haskell.org/?hoogle={}',
     'syn': 'https://powerthesaurus.com/{}/synonyms',
     'y': 'https://invidious.snopyta.org/search?q={}',
-    'yt': 'https://www.youtube.com/results?search_query={}'
+    'yt': 'https://www.youtube.com/results?search_query={}',
+    'q': 'https://www.qwant.com/?q=%s{}=web'
 }
+
+c.downloads.location.directory = '~/Downloads'
+c.content.fullscreen.window = True
+c.content.autoplay = False
+c.window.title_format = '{perc}{current_title}' 
+
+c.downloads.location.directory = '~/Downloads'
+
+# When to show the tab bar.
+# Valid values:
+#   - always: Always show the tab bar.
+#   - never: Always hide the tab bar.
+#   - multiple: Hide the tab bar if only one tab is open.
+#   - switching: Show the tab bar when switching tabs.
+c.tabs.show = 'multiple'
 
 c.content.blocking.adblock.lists = [ \
         "https://easylist.to/easylist/easylist.txt", \
@@ -129,127 +70,38 @@ c.content.blocking.method = 'both'
 
 # Text color of the completion widget. May be a single color to use for
 # all columns or a list of three colors, one for each column.
-# Type: List of QtColor, or QtColor
 c.colors.completion.fg = ['#f1f1f1', '#f1f1f1', '#f1f1f1']
 
-# Background color of the completion widget for odd rows.
-# Type: QssColor
 c.colors.completion.odd.bg = '#000000'
-
-# Background color of the completion widget for even rows.
-# Type: QssColor
 c.colors.completion.even.bg = '#131313'
-
-# Foreground color of completion widget category headers.
-# Type: QtColor
 c.colors.completion.category.fg = '#EBEBEB'
-
-# Background color of the completion widget category headers.
-# Type: QssColor
 c.colors.completion.category.bg = '#000000'
-
-# Top border color of the completion widget category headers.
-# Type: QssColor
 c.colors.completion.category.border.top = '#3f4147'
-
-# Bottom border color of the completion widget category headers.
-# Type: QssColor
 c.colors.completion.category.border.bottom = '#3f4147'
-
-# Foreground color of the selected completion item.
-# Type: QtColor
 c.colors.completion.item.selected.fg = '#f1f1f1'
-
-# Background color of the selected completion item.
-# Type: QssColor
 c.colors.completion.item.selected.bg = '#404040'
-
-# Foreground color of the matched text in the selected completion item.
-# Type: QtColor
 c.colors.completion.item.selected.match.fg = '#5f87af'
-
-# Foreground color of the matched text in the completion.
-# Type: QtColor
 c.colors.completion.match.fg = '#5f87af'
-
-# Color of the scrollbar handle in the completion view.
-# Type: QssColor
 c.colors.completion.scrollbar.fg = '#f1f1f1'
-
-# Background color for the download bar.
-# Type: QssColor
 c.colors.downloads.bar.bg = '#000000'
-
-# Background color for downloads with errors.
-# Type: QtColor
 c.colors.downloads.error.bg = '#ff6c6b'
-
-# Font color for hints.
-# Type: QssColor
-c.colors.hints.fg = '#000000'
-
-# Font color for the matched part of hints.
-# Type: QtColor
-c.colors.hints.match.fg = '#f1f1f1'
-
-# Background color of an info message.
-# Type: QssColor
+c.colors.hints.fg = '#ffffff'
+c.colors.hints.bg = '#000000'
+c.colors.hints.match.fg = '#999999'
 c.colors.messages.info.bg = '#000000'
-
-# Background color of the statusbar.
-# Type: QssColor
 c.colors.statusbar.normal.bg = '#000000'
-
-# Foreground color of the statusbar in insert mode.
-# Type: QssColor
 c.colors.statusbar.insert.fg = '#f1f1f1'
-
-# Background color of the statusbar in insert mode.
-# Type: QssColor
 c.colors.statusbar.insert.bg = '#000000'
-
-# Background color of the statusbar in passthrough mode.
-# Type: QssColor
 c.colors.statusbar.passthrough.bg = '#000000'
-
-# Background color of the statusbar in command mode.
-# Type: QssColor
 c.colors.statusbar.command.bg = '#000000'
-
-# Foreground color of the URL in the statusbar when there's a warning.
-# Type: QssColor
 c.colors.statusbar.url.warn.fg = '#ECBE7B'
-
-# Background color of the tab bar.
-# Type: QssColor
 c.colors.tabs.bar.bg = '#000000'
-
-# Background color of unselected odd tabs.
-# Type: QtColor
 c.colors.tabs.odd.bg = '#000000'
-
-# Background color of unselected even tabs.
-# Type: QtColor
 c.colors.tabs.even.bg = '#282c34'
-
-# Background color of selected odd tabs.
-# Type: QtColor
 c.colors.tabs.selected.odd.bg = '#242424'
-
-# Background color of selected even tabs.
-# Type: QtColor
 c.colors.tabs.selected.even.bg = '#242424'
-
-# Background color of pinned unselected odd tabs.
-# Type: QtColor
 c.colors.tabs.pinned.odd.bg = '#202020'
-
-# Background color of pinned unselected even tabs.
-# Type: QtColor
 c.colors.tabs.pinned.even.bg = '#000000'
-
-# Background color of pinned selected odd tabs.
-# Type: QtColor
 c.colors.tabs.pinned.selected.odd.bg = '#242424'
 
 ## Position of the tab bar.
@@ -260,48 +112,87 @@ c.colors.tabs.pinned.selected.odd.bg = '#242424'
 ##   - left
 ##   - right
 c.tabs.position = 'top'
-
-# Background color of pinned selected even tabs.
-# Type: QtColor
 c.colors.tabs.pinned.selected.even.bg = '#000000'
-
-# Default font families to use. Whenever "default_family" is used in a
-# font setting, it's replaced with the fonts listed here. If set to an
-# empty value, a system-specific monospace default is used.
-# Type: List of Font, or Font
 c.fonts.default_family = '"JetBrains Mono"'
-
-# Default font size to use. Whenever "default_size" is used in a font
-# setting, it's replaced with the size listed here. Valid values are
-# either a float value with a "pt" suffix, or an integer value with a
-# "px" suffix.
-# Type: String
 c.fonts.default_size = '16pt'
-
-# Font used in the completion widget.
-# Type: Font
 c.fonts.completion.entry = '16pt "JetBrains Mono"'
-
-# Font used for the debugging console.
-# Type: Font
 c.fonts.debug_console = 'default_size "JetBrains Mono"'
-
-# Font used for prompts.
-# Type: Font
 c.fonts.prompts = 'default_size sans-serif'
-
-# Font used in the statusbar.
-# Type: Font
 c.fonts.statusbar = '16pt "JetBrains Mono"'
 
-# Bindings to use dmenu rather than qutebrowser's builtin search.
-#config.bind('o', 'spawn --userscript dmenu-open')
-#config.bind('O', 'spawn --userscript dmenu-open --tab')
+# ---
+
+def bind(key, command, mode):  # noqa: E302
+    config.bind(key, command, mode=mode)
+
+def nmap(key, command):
+    bind(key, command, 'normal')
+
+def imap(key, command):
+    bind(key, command, 'insert')
+
+def cmap(key, command):
+    bind(key, command, 'command')
+
+def tmap(key, command):
+    bind(key, command, 'caret')
+
+def pmap(key, command):
+    bind(key, command, 'passthrough')
+
+def unmap(key, mode):
+    config.unbind(key, mode=mode)
+
+def nunmap(key):
+    unmap(key, mode='normal')
+
+# ** Quickmarks and Marks
+nunmap("'")
+# real quickmarks
+nmap("'51", ':open --tab localhost:5173')
+nmap("'zk", ':open --tab zedkaido.com')
+nmap("'sv", ':open --tab https://kit.svelte.dev/docs/')
+nmap("'gh", ':open --tab https://github.com')
+
+c.aliases = {'q': 'quit', 'w': 'session-save', 'wq': 'quit --save'}
+c.aliases['archive'] = 'open --tab https://web.archive.org/save/{url}'
+c.aliases['view-archive'] = 'open --tab https://web.archive.org/web/*/{url}'
+c.aliases['va'] = 'open --tab https://web.archive.org/web/*/{url}'
+c.aliases['view-google-cache'] = 'open https://www.google.com/search?q=cache:{url}'
+c.aliases['vgc'] = 'open https://www.google.com/search?q=cache:{url}'
 
 # Bindings for normal mode
-config.bind('M', 'hint links spawn mpv {hint-url}')
-config.bind('Z', 'hint links spawn st -e youtube-dl {hint-url}')
-config.bind('t', 'set-cmd-text -s :open -t')
-config.bind('xb', 'config-cycle statusbar.show always never')
-config.bind('xt', 'config-cycle tabs.show always never')
-config.bind('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
+('M', 'hint links spawn mpv {hint-url}')
+('Z', 'hint links spawn st -e youtube-dl {hint-url}')
+('t', 'set-cmd-text -s :open -t')
+('xb', 'config-cycle statusbar.show always never')
+('xt', 'config-cycle tabs.show always never')
+('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
+
+# Type: String
+# Valid values:
+#   - all: Accept all cookies.
+#   - no-3rdparty: Accept cookies from the same origin only. This is known to break some sites, such as GMail.
+#   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
+#   - never: Don't accept cookies at all.
+config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
+config.set('content.cookies.accept', 'all', 'devtools://*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://accounts.google.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36', 'https://*.slack.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://docs.google.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://drive.google.com/*')
+
+# Load images automatically in web pages.
+config.set('content.images', True, 'chrome-devtools://*')
+config.set('content.images', True, 'devtools://*')
+
+# Enable JavaScript.
+config.set('content.javascript.enabled', True, 'chrome-devtools://*')
+config.set('content.javascript.enabled', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome://*/*')
+config.set('content.javascript.enabled', True, 'qute://*/*')
+
+config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
+config.set('content.cookies.accept', 'all', 'devtools://*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
